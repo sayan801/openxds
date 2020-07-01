6c839e1d891523af894468769214ce387a2945aa
mbm@arosii.dk <mbm@arosii.dk@3203d48c-b89f-428c-a2b9-71f4dc330d7c>
2020-07-01 09:02:46 +0000

    * Repository-item table belongs in registry-database * Added debugging options to container in development-mode * Separated logs from registry-container and repository-container
    git-svn-id: https://svn.nspop.dk/svn/components/openxds/trunk@3570 3203d48c-b89f-428c-a2b9-71f4dc330d7c

--- compose/database/registry-data-db/01_openxds.sql
+++ compose/database/registry-data-db/01_openxds.sql
@@ -576,6 +576,21 @@ CREATE TABLE `registrypackage` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `repositoryitem`
+--
+
+DROP TABLE IF EXISTS `repositoryitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `repositoryitem` (
+  `lid` varchar(256) NOT NULL,
+  `versionname` varchar(16) NOT NULL,
+  `content` mediumblob,
+  PRIMARY KEY (`lid`,`versionname`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `service`
